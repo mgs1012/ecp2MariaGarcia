fc8d862c9e3f87a73d00708d6fed12a7e282dc79
mariaGarciaSalazar <maria.gsalazar@alumnos.upm.es>
2014-10-02 11:17:02 +0200

    Prueba travis

---
+++ src/test/java/es/upm/miw/spai/ecp2/DecimalCollectionTest.java
@@ -0,0 +1,48 @@
+package es.upm.miw.spai.ecp2;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class DecimalCollectionTest {
+
+    private DecimalCollection collection;
+
+    @Before
+    public void before() {
+        this.collection = new DecimalCollection();
+        this.collection.add(0.0);
+        this.collection.add(1.2);
+        this.collection.add(2.8);
+    }
+
+    @Test
+    public void testDecimalCollection() {
+        this.collection = new DecimalCollection();
+        assertEquals(0, this.collection.size());
+    }
+
+    @Test
+    public void testAdd() {
+        this.collection = new DecimalCollection();
+        this.collection.add(2.2);
+        assertEquals(2.2, this.collection.sum(), 10e-3);
+    }
+
+    @Test
+    public void testSize() {
+        assertEquals(3, this.collection.size());
+    }
+
+    @Test
+    public void testSum() {
+        assertEquals(4.0, this.collection.sum(), 10e-3);
+    }
+
+    @Test
+    public void testHigher() {
+        assertEquals(2.8, this.collection.higher(), 10e-3);
+    }
+
+}
---
+++ src/test/java/es/upm/miw/spai/ecp2/UserTest.java
@@ -0,0 +1,29 @@
+package es.upm.miw.spai.ecp2;
+
+import static org.junit.Assert.*;
+import org.junit.Before;
+import org.junit.Test;
+
+public class UserTest {
+
+    private User user;
+
+    @Before
+    public void before() {
+        user = new User(8, "María", "Garcia");
+    }
+
+    @Test
+    public void testUser() {
+        assertEquals(8, this.user.getNumber());
+        assertEquals("María", this.user.getName());
+        assertEquals("Garcia", this.user.getFamilyName());
+    }
+
+    @Test
+    public void testFullName() {
+        assertEquals("María Garcia", this.user.fullName());
+    }
+
+  
+}
